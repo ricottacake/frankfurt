7da7c58ffc97ee21a2ac301fe4599a3bcfd30223
Kirill Vereshchak <20265124+ricottacake@users.noreply.github.com>
2023-01-05 00:06:11 +0200

    refactor: AppConfig

--- src/main/java/com/aceliq/frankfurt/components/AppConfig.java
+++ src/main/java/com/aceliq/frankfurt/components/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import com.aceliq.frankfurt.models.User;
+import com.aceliq.frankfurt.models.Card;
 import com.aceliq.frankfurt.models.Deck;
 import com.aceliq.frankfurt.models.TimeIsOver;
 
@@ -19,5 +20,15 @@ public class AppConfig {
   @Bean
   public Deck deck() {
     return new Deck();
+  }
+  
+  @Bean
+  public User user() {
+    return new User();
+  }
+  
+  @Bean
+  public Card card() {
+    return new Card();
   }
 }
