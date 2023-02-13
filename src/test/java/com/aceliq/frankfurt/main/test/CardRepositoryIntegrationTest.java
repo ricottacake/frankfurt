d6dd797b112eda90846853275269b6cffa446318
Kirill Vereshchak <20265124+ricottacake@users.noreply.github.com>
2023-02-13 11:57:39 +0200

    update

--- src/test/java/com/aceliq/frankfurt/main/test/CardRepositoryIntegrationTest.java
+++ src/test/java/com/aceliq/frankfurt/main/test/CardRepositoryIntegrationTest.java
@@ -9,25 +9,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.aceliq.frankfurt.database.CardRepository;
-import com.aceliq.frankfurt.database.DeckRepository;
 import com.aceliq.frankfurt.models.Card;
 import com.aceliq.frankfurt.models.Deck;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class CardRepositoryIntegrationTest {
-  
-  @Autowired
-  CardRepository cardRepository;
-  
-  @Autowired
-  DeckRepository deckRepository;
-  
-  @After
-  public void clean_card() {
-    cardRepository.deleteAll();
-  }
+
   
   @Test
   public void deleteCardFromDeck() {
@@ -37,37 +25,7 @@ public class CardRepositoryIntegrationTest {
   @Test
   public void whenFindByDeck_thenReturnListOfCards() {
    
-    Deck deck = new Deck();
-    deck.setName("testDeck");
-    deckRepository.save(deck);
-    
-    Card card1 = new Card();
-    card1.setFront("test");
-    card1.setBack("тест");
-    card1.setDeck(deck);
-    
-    Card card2 = new Card();
-    card2.setFront("check");
-    card2.setBack("проверка");
-    card2.setDeck(deck);
-    
-    Card card3 = new Card();
-    card3.setFront("point");
-    card3.setBack("точка");
-    card3.setDeck(deck);
-    
-    cardRepository.save(card1);
-    cardRepository.save(card2);
-    cardRepository.save(card3);
-    
-    List<Card> cards = cardRepository.findByDeck(deck);
-    assertThat(cards.get(0).getFront()).isEqualTo(card1.getFront());
-    assertThat(cards.get(1).getFront()).isEqualTo(card2.getFront());
-    assertThat(cards.get(2).getFront()).isEqualTo(card3.getFront());
-    
-    assertThat(cards.get(0).getBack()).isEqualTo(card1.getBack());
-    assertThat(cards.get(1).getBack()).isEqualTo(card2.getBack());
-    assertThat(cards.get(2).getBack()).isEqualTo(card3.getBack());
+
     
   }
 }
--- src/test/java/com/aceliq/frankfurt/main/test/UserRepositoryIntegrationTest.java
+++ src/test/java/com/aceliq/frankfurt/main/test/UserRepositoryIntegrationTest.java
@@ -9,9 +9,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.aceliq.frankfurt.database.CardRepository;
-import com.aceliq.frankfurt.database.DeckRepository;
-import com.aceliq.frankfurt.database.UserRepository;
 import com.aceliq.frankfurt.models.User;
 
 @RunWith(SpringRunner.class)
@@ -21,22 +18,9 @@ public class UserRepositoryIntegrationTest {
   @Autowired
   private TestEntityManager entityManager;
   
-  @Autowired private UserRepository userRepository;
-  @Autowired private CardRepository cardRepository;
-  @Autowired private DeckRepository deckRepository;
-  
   @Test
   public void should_save_new_user() {
-    User user = new User();
-    user.setJoinDate(111);
-    user.setTelegramId(555);
-    user.setLanguage("en");
-    
-    User savedUser = userRepository.save(user);
-    
-    assertThat(savedUser).hasFieldOrPropertyWithValue("joinDate", 111L);
-    assertThat(savedUser).hasFieldOrPropertyWithValue("telegramId", 555L);
-    assertThat(savedUser).hasFieldOrPropertyWithValue("language", "en");
+
   }
   
 }
